23fcf3cee56d4e4e1734fd2ddbee40c2d2305457
unknown <davit.kurdadze17b@geolab.edu.ge>
2021-07-19 01:30:17 +0400

    feat: implement checkbox selectAll, clearAll, CheckboxChange

--- src/components/content/MainContent.js
+++ src/components/content/MainContent.js
@@ -1,16 +1,40 @@
 import "./MainContent.css";
 import CatalogNav from "../catalogNav/CatalogNav"
 import CatalogContent from "./catalog/CatalogContent";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
 
 
 const MainContent = () => {
+  const [checkedProducts, setCheckedProducts] = useState([]);
+  const productList = useSelector(state => state.ProductsReducer.productList);
 
   
+  const checkboxChanged = (id) => {
+    if (checkedProducts.includes(id)) {
+      setCheckedProducts(checkedProducts.filter((i) => i !== id));
+    } else setCheckedProducts([...checkedProducts, id]);
+  };
+
+  const selectAll = () => {
+    setCheckedProducts(productList.map((p) => p.id));
+  };
+
+  const clearAll = () => {
+    setCheckedProducts([]);
+  };
+
   return (
     <div className="main">
       <div className="main__catalog">
         <CatalogNav />
-        <CatalogContent />
+        <CatalogContent 
+          checkedProducts={checkedProducts}
+          checkboxChanged={checkboxChanged}
+          selectAll={selectAll}
+          clearAll={clearAll}
+        />
       </div>
     </div>
   );
