c0ca42190ad15a5c8fce1bcb849da92b14425b97
unknown <davit.kurdadze17b@geolab.edu.ge>
2021-07-21 19:55:56 +0400

    feat: add actions folder/file

---
+++ src/redux/actions/actionTypes.js
@@ -0,0 +1,28 @@
+export const ActionTypes = {
+    PRODUCTS_FETCH: "PRODUCTS_FETCH",
+    SELECTED_PRODUCTS: "SELECTED_PRODUCTS",
+    RM_SELECTED_PRODUCTS: "RM_SELECTED_PRODUCTS",
+    PRODUCTS_SEARCH: "PRODUCTS_SEARCH",
+    SORT_CHANGE: "SORT_CHANGE"
+}
+
+export const setProducts = (products) => {
+    return {
+        type: ActionTypes.PRODUCTS_FETCH,
+        payload: products
+    }
+}
+
+export const sortAction = (data) => {
+    return {
+        type: ActionTypes.SORT_CHANGE,
+        payload: data
+    }
+}
+
+export const searchAction = (data) => {
+    return {
+        type: ActionTypes.PRODUCTS_SEARCH,
+        payload: data
+    }
+}
